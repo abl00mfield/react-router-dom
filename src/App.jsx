159caee07d6843620727b8470683aa654588b551
abl00mfield <amanda.bloomfield@gmail.com>
2025-03-28 15:14:47 -0600

    added form and finished lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
-import "./App.css";
+import { Route, Routes } from "react-router";
+import PokemonDetails from "./components/PokemonDetails/PokemonDetails";
+// import "./App.css";
 import PokemonList from "./components/PokemonList/PokemonList";
 import NavBar from "./components/NavBar/NavBar";
+import PokemonForm from "./components/PokemonForm";
 
 const initialState = [
   { _id: 1, name: "bulbasaur", weight: 69, height: 7 },
@@ -13,11 +16,29 @@ const initialState = [
 
 function App() {
   const [pokemon, setPokemon] = useState(initialState);
+
+  const addPokemon = (newPokemonData) => {
+    newPokemonData._id = pokemon.length + 1;
+    setPokemon([...pokemon, newPokemonData]);
+  };
+
   return (
     <>
+      <h1>Pokemon!</h1>
       <NavBar />
-      <h1>Pokeman!</h1>
-      <PokemonList pokemon={pokemon} />
+      <Routes>
+        <Route path="/" element={<h2>Home Page</h2>} />
+        <Route path="/pokemon" element={<PokemonList pokemon={pokemon} />} />
+        <Route
+          path="/pokemon/new"
+          element={<PokemonForm addPokemon={addPokemon} />}
+        />
+        <Route
+          path="/pokemon/:pokemonId"
+          element={<PokemonDetails pokemon={pokemon} />}
+        />
+        <Route path="*" element={<h2>Whoops, nothing here</h2>} />
+      </Routes>
     </>
   );
 }
